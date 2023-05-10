c45d57c9b89bff07fdc4424bf9888d8d0a4f1a10
Viktor Pakhai <vpakhai@softserveinc.com>
2023-05-10 13:25:49 +0300

    Change deploy stage

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ASG_STAGE="asg_stage"
+AWS_REGION="eu-central-1"
+
+
+
+
+# Retrieve the instance IDs associated with the Auto Scaling Group
+instance_ids=$(aws autoscaling describe-auto-scaling-instances --profile private --query "AutoScalingInstances[?AutoScalingGroupName=='$ASG_STAGE'].InstanceId" --output text)
+
+# Print the list of instance IDs
+echo "Instance IDs:"
+echo "$instance_ids"
+
+COMANDS = "touch /home/ubuntu/test555.txt"
+
+# Execute AWS SSM command on each instance
+IFS=$'\n' read -rd '' -a instance_array <<< "$instance_ids"
+for instance_id in "${instance_array[@]}"; do
+    echo "Processing instance: $instance_id"
+
+    # Escape the instance ID
+    escaped_instance_id=$(printf '%s' "$instance_id" | sed -e 's/"/\\"/g')
+	echo "$escaped_instance_id"
+
+    # Execute AWS SSM command on the instance
+    aws ssm send-command \
+        --profile private \
+        --region $AWS_REGION \
+        --instance-ids $instance_id \
+        --document-name "AWS-RunShellScript" \
+        --parameters 'commands= ["echo bla-bla && \
+		touch /home/ubuntu/ssm.txt && \
+		ls -la
+
+
+
+		"]'
+done
