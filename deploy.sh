2f673dc1ff6ca63b3705091458f2d137116cb855
Viktor Pakhai <vpakhai@softserveinc.com>
2023-05-10 17:55:24 +0300

    Change deploy stage

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
 
 
-
-ASG_STAGE="asg_stage"
-AWS_REGION="eu-central-1"
-
-
-
+ASG=$1
+DOCKER_IMMAGE=$2
+AWS_REGION=$3
 
 # Retrieve the instance IDs associated with the Auto Scaling Group
-instance_ids=$(aws autoscaling describe-auto-scaling-instances --profile private --query "AutoScalingInstances[?AutoScalingGroupName=='$ASG_STAGE'].InstanceId" --output text)
+instance_ids=$(aws autoscaling describe-auto-scaling-instances --profile private --query "AutoScalingInstances[?AutoScalingGroupName=='$ASG'].InstanceId" --output text)
 
-# Print the list of instance IDs
-echo "Instance IDs:"
-echo "$instance_ids"
 
-RUN_COMMANDS_SSM= "touch /home/ubuntu/test555.txt"
+RUN_COMMANDS="aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 882166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo &&\
+              docker pull 882166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo:latest &&\
+              docker run -d -p 80:8000 882166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo:latest"
 
 # Execute AWS SSM command on each instance
 IFS=$'\n' read -rd '' -a instance_array <<< "$instance_ids"
 for instance_id in "${instance_array[@]}"; do
-    echo "Processing instance: $instance_id"
-
-    # Escape the instance ID
-    escaped_instance_id=$(printf '%s' "$instance_id" | sed -e 's/"/\\"/g')
-	echo "$escaped_instance_id"
-
-    # Execute AWS SSM command on the instance
-    aws ssm send-command \
-        --profile private \
-        --region $AWS_REGION \
-        --instance-ids $instance_id \
-        --document-name "AWS-RunShellScript" \
-        --parameters 'commands=$RUN_COMMANDS_SSM '
+    aws ssm send-command --profile private --region $AWS_REGION --instance-ids $instance_id --document-name AWS-RunShellScript --parameters "commands=$RUN_COMMANDS"
 done
