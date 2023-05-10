e7cc945852b7a7cd205d8fb1ea1eea39fc2eb3dc
Viktor Pakhai <vpakhai@softserveinc.com>
2023-05-10 19:32:20 +0300

    Change deploy stage

--- deploy.sh
+++ deploy.sh
@@ -19,5 +19,5 @@ RUN_COMMANDS="aws ecr get-login-password --region eu-central-1 | docker login --
 # Execute AWS SSM command on each instance
 IFS=$'\n' read -rd '' -a instance_array <<< "$instance_ids"
 for instance_id in "${instance_array[@]}"; do
-    aws ssm send-command --profile private --region $AWS_REGION --instance-ids $instance_id --document-name AWS-RunShellScript --parameters "commands=$RUN_COMMANDS"
+    aws ssm send-command  --region $AWS_REGION --instance-ids $instance_id --document-name AWS-RunShellScript --parameters "commands=$RUN_COMMANDS"
 done
