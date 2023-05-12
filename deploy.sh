8f799393a3c444ced18f0a943d0273fbce6d0ec8
Viktor Pakhai <vpakhai@softserveinc.com>
2023-05-12 18:01:02 +0300

    Changed v1 of GH actions

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-
-IMAGE="82166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo"
-
-
 # Retrieve the instance IDs associated with the Auto Scaling Group
+
 instance_ids=$(aws autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?AutoScalingGroupName=='$ASG'].InstanceId" --output text)
 echo "APP will be deployed to: $instance_ids"
 
-RUN_COMMANDS="aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 882166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo &&\
+RUN_COMMANDS="aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin 882166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo &&\
               echo "$IMAGE" > /home/ubuntu/image_name.txt &&\
               docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest &&\
               docker stop ci-cd-app || true &&\
