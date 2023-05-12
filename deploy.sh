21d30eb29b1a5dec8bfb02ce8a573820359bab8e
Viktor Pakhai <vpakhai@softserveinc.com>
2023-05-12 17:20:27 +0300

    Add changes tu RUN command deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 ASG="asg_stage"
+IMAGE="82166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo"
+
 
 # Retrieve the instance IDs associated with the Auto Scaling Group
 instance_ids=$(aws autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?AutoScalingGroupName=='$ASG'].InstanceId" --output text)
 echo "APP will be deployed to: $instance_ids"
 
 RUN_COMMANDS="aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 882166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo &&\
-              IMAGE="82166133385.dkr.ecr.eu-central-1.amazonaws.com/nebo-repo" &&\
-              echo "$IMAGE" >> /home/ubuntu/image_name.txt
+              echo "$IMAGE" > /home/ubuntu/image_name.txt &&\
               docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest &&\
               docker stop $ECR_REGISTRY/$ECR_REPOSITORY:latest || true &&\
               docker rm $ECR_REGISTRY/$ECR_REPOSITORY:latest   || true &&\
-              docker run -d --name PythonAPP -p 80:8000 -it $ECR_REGISTRY/$ECR_REPOSITORY:latest"
+              docker run -d --name ci-cd-app -p 80:8000 -it $ECR_REGISTRY/$ECR_REPOSITORY:latest"
 
 echo "Run_Command passed "
 # Execute AWS SSM command on each instance
