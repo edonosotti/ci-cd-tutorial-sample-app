b2ff02c2c6a3e4e9d8f6a66f5d521dde51a42fd5
Viktor Pakhai <vpakhai@softserveinc.com>
2023-05-12 14:12:50 +0300

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-
 ASG=$1
 #DOCKER_IMMAGE=$2
 #AWS_REGION=$3
 
 echo "$ASG"
-
-
-
 # Retrieve the instance IDs associated with the Auto Scaling Group
 instance_ids=$(aws autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?AutoScalingGroupName=='$ASG'].InstanceId" --output text)
 
@@ -17,7 +13,7 @@ RUN_COMMANDS="aws ecr get-login-password --region eu-central-1 | docker login --
               docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest &&\
               docker run -d -p 80:8000 $ECR_REGISTRY/$ECR_REPOSITORY:latest"
 
-
+echo "Run_Command passed"
 # Execute AWS SSM command on each instance
 IFS=$'\n' read -rd '' -a instance_array <<< "$instance_ids"
 for instance_id in "${instance_array[@]}"; do
