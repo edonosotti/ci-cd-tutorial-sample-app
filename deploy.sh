e0c3f5264bd203c3c494940f3c81ff8210cf944f
Viktor Pakhai <vpakhai@softserveinc.com>
2023-05-12 17:32:42 +0300

    Add changes tu RUN command deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ RUN_COMMANDS="aws ecr get-login-password --region eu-central-1 | docker login --
               docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest &&\
               docker stop ci-cd-app || true &&\
               docker rm ci-cd-app   || true &&\
-              docker run -d --name ci-cd-app -p 80:8000 -it $ECR_REGISTRY/$ECR_REPOSITORY:latest --rm $IMAGE"
+              docker run -d --name ci-cd-app -p 80:8000 -it $ECR_REGISTRY/$ECR_REPOSITORY:latest"
 
 echo "Run_Command passed "
 # Execute AWS SSM command on each instance
