bd0ae21033b6492e192e783802e3a7605681d212
ZahrunFairooz <123552136+ZahrunFairooz@users.noreply.github.com>
2025-07-28 17:58:33 +0530

    Zahrun

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,12 @@ from app import db
 from app.models import Menu
 
 menu_items = [
-    {"id": 1, "name": "Pizza", "price": 1200},
-    {"id": 2, "name": "Burger", "price": 900}
-]
-
+    {"id": 1, "name": "Pizza", "price": 1200, "quantity": 10},
+    {"id": 2, "name": "Burger", "price": 900, "quantity": 5},
+    {"id": 3, "name": "Fried Rice", "price": 800, "quantity": 7},
+    {"id": 4, "name": "Pasta", "price": 1000, "quantity": 6},
+    {"id": 5, "name": "Sandwich", "price": 650, "quantity": 8},
+    {"id": 6, "name": "French Fries", "price": 400, "quantity": 15}]
 @app.route('/')
 def home():
 	return jsonify({ "status": "ok" })
